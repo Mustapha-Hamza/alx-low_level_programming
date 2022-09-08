b05a3e2673de57549b35b9901866f16426503d75
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-08 12:53:14 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 
-	return (0);
+return (0);
 }	
