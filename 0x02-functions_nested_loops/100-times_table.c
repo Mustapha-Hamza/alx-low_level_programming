c006a428734d05ab0d050e1a74789eee121bde8f
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-13 15:20:43 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,6 @@
 void print_times_table(int n)
 {
 	int row, column, product;
-	
 	if (n >= 0 && n < 15)
 	{
 		for (row = 0; row <= n; row++)
@@ -20,7 +19,6 @@ void print_times_table(int n)
 			for (column = 0; column <= n; column++)
 			{
 				product = row * column;
-				
 				if (column == 0)
 				_putchar('0');
 				else if (product < 10)
@@ -37,7 +35,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) % 10 + '0');
-					_putchar(product %10 + '0');
+					_putchar(product % 10 + '0');
 				}
 				else if (product > 99 && product < 1000)
 				{
