2bf4bd850cb93c912db7146f35b1ed97abba871b
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-13 10:52:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,13 +7,13 @@ void print_alphabet_x10(void);
 int _isolower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int -abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times-table(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void print-times_tablet(int n);
+void print_times_tablet(int n);
 void num(int r, int n);
 
 #endif
