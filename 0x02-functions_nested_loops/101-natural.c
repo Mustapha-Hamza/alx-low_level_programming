d5840d7c27421be1359db5b00682bfa3c6887361
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-13 15:37:47 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int num, sum;
+
 	sum = 0;
 	for (num = 0; num < 1024; num++)
 				{
@@ -17,7 +18,6 @@ int main(void)
 						sum += num;
 					}
 				}
-				
 				printf("%d\n", sum);
 
 				return (0);
