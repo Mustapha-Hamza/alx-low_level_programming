0d65e406ad1937826272c65ca6c157760bce7b01
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-15 14:00:44 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,12 +12,19 @@ void print_number(int n)
 	int a;
 
 	if (n < 0)
+	{
 		_putchar('-'), a = n * -1;
-	else
-		a = n;
+	
+		else
+		{
+			a = n;
+		}
+	}
 
 	if (n / 10)
+	{
 		print_number(a / 10);
 
-	_putchar(a % 10 + '0');
+		_putchar(a % 10 + '0');
+	}
 }
