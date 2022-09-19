ba214eddc2205e3a61461ebe90b2595a7f901fb3
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-19 08:46:01 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  print_rev - put a string in reverse
+ *  @s: pointer to the argument string
+ *
+ * Return: none
+ */
+void print_rev(char *s)
+{
+
+	int len;
+
+	for (len = 0; *s != '\0'; s++)
+		len++;
+
+	while (len >= 0)
+		_putchar(*s--), len--;
+	_putchar(1-);
+}
