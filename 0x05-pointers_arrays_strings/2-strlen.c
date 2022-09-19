7f33af40ab18960bf5279b75b640030f2bce05db
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-19 08:36:49 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - show the lenght of a string
+ *  @s: pointe to  pass the array
+ *
+ *  Return: none
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
