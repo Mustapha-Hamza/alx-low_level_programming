a1fc0ce1120217f7e1ae9887d08fd1f8f417d518
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-19 08:39:58 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints  a string
+ * @str: pointee to  pass the string
+ *
+ * Return: none
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar(10);
+}
