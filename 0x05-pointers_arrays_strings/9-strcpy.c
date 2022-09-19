90394259a02103739ca90f75abf9db7dd57976a7
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-19 09:32:24 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy a string from another pointer
+ * @dest: pointer to a string
+ * @src: pointer to the strign to concatenate
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *cp = dest;
+
+	while ((*dest++ = *src++))
+		;
+	dest = cp;
+	return (dest);
+}
