545b30e907be50d61a911a50fadba0ff5913b729
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-09-19 09:19:51 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_array - print array
+ * @a: array
+ * @n: number iteration
+ * Return: none
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n && (*a + 1) != '\0'; i++)
+		printf("%i", *a++);
+	printf("%i\n", *a);
+}
