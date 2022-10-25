52dd0172fe4d011687021fd7f2f31f4d243e4030
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-10-25 09:30:17 -0700

    hotfix

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,23 +11,31 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow, *fast;
-
-	slow = head;
-	fast = head;
-	while (head != NULL)
-	{
-		if (slow == fast)
-		{
-			while (head != NULL)
-			{
-				if (slow == fast)
-					return (slow);
-				slow = slow->next;
-				fast = fast->next;
-			}
-		}
-		slow = slow->next;
-		fast = fast->next->next;
-	}
+		listint_t *tmp = NULL;
+			data[1024];
+				long int  i = 0;
+					
+					while (head != NULL)
+							{
+										if (head->n - 1 == head->next->n)
+													{
+																	data[i] = &head;
+																				i++;
+																							head = head->next;
+																									} head = head->next;
+												i++;
+													}
+						while (i >= 0)
+								{
+											if (data[i] + 1 == data[i - 1]);
+													{
+																	i++;
+																				continue;
+																						}
+															else
+																		{
+																						return (NULL);
+																								}
+																}
+							return ((listint_t *) data[0]);
 }
