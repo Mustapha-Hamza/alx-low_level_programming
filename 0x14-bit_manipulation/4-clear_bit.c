fd1584993874748860c191e200fe0e1c50458b12
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2022-10-27 11:22:51 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,21 +5,20 @@
  * @n: The unsigned long int to print in binary
  * @index: The index at which to set the value to 0
  *
- * Return: 1 if it succeeds, -1 if it fails
+ * Return: 1 if it worked, -1 if it didn't.
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int clear_bit(unsigned long int *n, unsigned int index)
+	unsigned int m;
 
-		if (index >= (sizeof(*n) * 8))
-			return (-1);
-		mask = 1;
-		mask = mask << index;
-		mask = ~mask;
-		*n = *n & mask;
+	if (index > 63)
+		return (-1);
 
-		return (1);
+	m = 1 << index;
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
 }
 
 
